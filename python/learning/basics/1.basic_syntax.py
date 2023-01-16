d2a65a4f44435d57319977e0272798ad7a1caf72
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-01-15 20:07:39 -0500

    basics

---
+++ python/learning/basics/1.basic_syntax.py
@@ -0,0 +1,12 @@
+# we can print something on console with print() function
+print('this is a print in python')
+
+# creating a variable
+name = 'sneuder'
+number = 20
+# using f string
+print(f"{name} has {number} apples")
+
+# getting something from user
+input_user = input('type something you like: ')
+print(f"you print {input_user}")
---
+++ python/learning/basics/2.variables.py
@@ -0,0 +1,12 @@
+# knowing how old you are (type and casting)
+# we can use built function to make those castings
+current_year = 2023
+user_year = input('tell me when you were born: ')
+print(f"you are: {current_year - int(user_year)}")
+# we have more variable types, but these are the basics
+float("23.65")
+int("123")
+str(123)
+bool("False")
+
+# string methods
