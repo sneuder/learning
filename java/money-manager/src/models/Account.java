c27421b3a7f4300f0cb0e1fbfcbcee4b5294a324
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-03-12 21:41:04 -0500

    feat: accounts models

--- java/money-manager/src/models/Account.java
+++ java/money-manager/src/models/Account.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Account {
-  
+  String id;
+  String name;
+  float amount;
+  String description;
+  String groupAccount;  
+
+  public Account(String name, float amount, String description, String groupAccount ) {
+    this.name = name;
+    this.amount = amount;
+    this.description = description;
+    this.groupAccount = groupAccount;
+  }
 }
--- java/money-manager/src/models/GroupAccount.java
+++ java/money-manager/src/models/GroupAccount.java
@@ -1,5 +1,13 @@
 package models;
 
 public class GroupAccount {
-  
+  String id;
+  String name;
+  String description;
+  Account[] accounts;
+
+  public GroupAccount(String name, String description){
+    this.name = name;
+    this.description = description;
+  }
 }
